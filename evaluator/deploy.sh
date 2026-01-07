7bdf822fd0c793cfae072f48988459622d2d0fe3
Rick Viscomi <rviscomi@google.com>
2026-01-06 20:45:25 -0500

    faster deploy

--- evaluator/deploy.sh
+++ evaluator/deploy.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-rsync -av --exclude='.git' --exclude='node_modules' --exclude='.env' . /google/data/rw/users/rv/rviscomi/www/spike-test/
+rsync -avP --exclude='.git' --exclude='node_modules' --exclude='.env' --exclude='.DS_Store' . /google/data/rw/users/rv/rviscomi/www/spike-test/
